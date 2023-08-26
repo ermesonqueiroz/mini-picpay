4034246e6bcf8dd006e588a8f6feada313e148ee
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-08-26 20:00:15 -0300

    feat: initial config

---
+++ src/main/java/com/minipicpay/MinipicpayApplication.java
@@ -0,0 +1,11 @@
+package com.minipicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinipicpayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MinipicpayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/minipicpay/MinipicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minipicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinipicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
